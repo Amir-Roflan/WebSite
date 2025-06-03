6d899bf4884f2cd70168b078f306d5da40671ba9
Akhmetzyanov Amir <akhmetzyanovamir2006@gmail.com>
2025-06-03 16:26:13 +0300

    added pages, fotos, new css content and etc.

--- main.go
+++ main.go
@@ -5,15 +5,57 @@ import (
 	"net/http"
 )
 
-func homepage(w http.ResponseWriter, r *http.Request){
-	tmpl, err := template.ParseFiles("templates/home_page.html", "templates/header.html", "templates/footer.html")
+func helpPage(w http.ResponseWriter, r *http.Request){
+	tmpl, err := template.ParseFiles("templates/help_page.html", "templates/header.html", "templates/footer.html")
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.ExecuteTemplate(w, "help_page", nil)
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func settingsPage(w http.ResponseWriter, r *http.Request){
+	tmpl, err := template.ParseFiles("templates/settings_page.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return 
 	}
 
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	err = tmpl.ExecuteTemplate(w, "settings_page", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError) 
+	}
+}
+
+func aboutPage(w http.ResponseWriter, r *http.Request){
+	tmpl, err := template.ParseFiles("templates/about_page.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return 
+	}
+
+	err = tmpl.ExecuteTemplate(w, "about_page", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError) 
+	}
+}
+
+func homePage(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/" {
+		http.NotFound(w, r) // добавлено только что 
+		return
+	}
+	
+
+	tmpl, err := template.ParseFiles("templates/home_page.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return 
+	}
 	
 	err = tmpl.ExecuteTemplate(w, "home_page", nil)
 	if err != nil{ 
@@ -22,7 +64,13 @@ func homepage(w http.ResponseWriter, r *http.Request){
 }
 
 func handleRequests(){
-	http.HandleFunc("/", homepage)
+	fs := http.FileServer(http.Dir("static")) // хранение статитечских данных
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/settings_page", settingsPage)
+	http.HandleFunc("/about_page", aboutPage)
+	http.HandleFunc("/help_page", helpPage)
 	http.ListenAndServe(":8080", nil)
 }
 
