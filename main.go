a600f0631c74ff2755036b8b71fd8c8c6c359b17
Akhmetzyanov Amir <akhmetzyanovamir2006@gmail.com>
2025-06-10 16:46:08 +0300

    added 2GIS API to pet-project

--- main.go
+++ main.go
@@ -13,13 +13,21 @@ import (
 
 const (
 	apiBaseURL  = "https://catalog.api.2gis.com/3.0/items"
-	apiImageURL = "https://catalog.api.2gis.com/3.0/items/images"
+	staticMapURL = "https://static.maps.2gis.com/1.0"
+	imageSize = "600x400"
+	zoom = "17"
 )
+
+type Point struct{
+	Lat 	 float64  `json:"lat"`	// создали структуру для координат потом засунули в Location 
+	Lon 	 float64  `json:"lon"`
+}
 
 type Location struct {
 	ID       string   `json:"id"`
 	Name     string   `json:"name"`
 	Address  string   `json:"address_name"`
+	Point 	 Point	  `json:"point"` // все координаты
 }
 
 type APIResponse struct {
@@ -29,17 +37,18 @@ type APIResponse struct {
 }
 
 func searchPage(w http.ResponseWriter, r *http.Request){
-
 	tmpl, err := template.ParseFiles("templates/search_page.html", "templates/header.html", "templates/footer.html")
 	if err != nil{
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
-	}
-	query := r.URL.Query().Get("q") // считывает данные со строки поиска...
+	} // инициализация страницы
+
+	query := r.URL.Query().Get("q") // считывает данные со строки поиска
 	if query == "" {
 		http.Error(w, "Нужно что-то ввести", http.StatusBadRequest)
 		return
 	}
+
 	locations, err := searchLocations(query) 
 	if err != nil{
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -50,13 +59,23 @@ func searchPage(w http.ResponseWriter, r *http.Request){
 		ID 		string 		
 		Name 	string 	
 		Address string	
+		Lat		float64 // обьявление координат
+		Lon 	float64
+		MapURL  string // статитческий URL карты
 	}
+
 	var viewLocations []ViewLocation
-	for _, loc := range locations{ // поменяли locations testLocations
+	
+	for _, loc := range locations{ 
+		mapURL := generateStaticMapURL(loc.Point.Lat, loc.Point.Lon) // функция которая генерирует URL для статической карты
+
 		vl := ViewLocation{
 			ID: 		loc.ID,
 			Name: 		loc.Name,
 			Address: 	loc.Address,
+			Lat: 		loc.Point.Lat,
+			Lon: 		loc.Point.Lon,
+			MapURL:     mapURL,
 		}
 		viewLocations = append(viewLocations, vl)
 	}
@@ -68,18 +87,24 @@ func searchPage(w http.ResponseWriter, r *http.Request){
 		Query:     query,
 		Locations: viewLocations,
 	}
+
 	err = tmpl.ExecuteTemplate(w, "search_page", data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func searchLocations(query string) ([]Location, error) {
+func generateStaticMapURL(lat, lon float64) string {
+	apiKey := os.Getenv("API_KEY")
+	return fmt.Sprintf("%s?zoom=%s&size=%s&center=%f,%f&key=%s",
+		staticMapURL, zoom, imageSize, lon, lat, apiKey)
+}
 
+func searchLocations(query string) ([]Location, error) { // добавил lon lat в функцию
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
-	}
+	} // получение API 
 	
 	apiKey := os.Getenv("API")
 
@@ -96,9 +121,11 @@ func searchLocations(query string) ([]Location, error) {
 		return nil, fmt.Errorf("2GIS API returned status %d: %s", resp.StatusCode, body)
 	}
 	var apiResponse APIResponse
+
 	if err := json.NewDecoder(resp.Body).Decode(&apiResponse); err != nil {
 		return nil, fmt.Errorf("failed to decode API response: %v", err)
 	}
+
 	log.Printf("Parsed %d locations", len(apiResponse.Result.Items))
 	return apiResponse.Result.Items, nil
 }
